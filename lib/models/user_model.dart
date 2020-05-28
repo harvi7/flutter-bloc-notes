0fe06315c91426eb7ee04524dd882fc286591fd6
Harsh Virani <iamharshvirani7@gmail.com>
2020-05-28 12:58:05 -0700

    User Entity and Model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,31 @@
+
+import 'package:equatable/equatable.dart';
+import 'package:bloc_notes/entities/user_entity.dart';
+import 'package:meta/meta.dart';
+
+class User extends Equatable {
+  final String id;
+  final String email;
+
+  const User({
+    @required this.id,
+    @required this.email,
+  });
+
+  @override
+  List<Object> get props => [id, email];
+
+  @override
+  String toString() => '''User {
+    id: $id,
+    email: $email
+  }''';
+
+  UserEntity toEntity() {
+    return UserEntity(id: id, email: email);
+  }
+
+  factory User.fromEntity(UserEntity entity) {
+    return User(id: entity.id, email: entity.email);
+  }
+}
